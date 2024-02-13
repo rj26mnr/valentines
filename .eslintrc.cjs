03449aaa8f4cba51d1e53f7b5ec2bbed2fecd90d
Ralph Jeremie Munar <rjmunar@myseneca.ca>
2024-02-13 10:51:57 -0500

    Update ESlint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,27 @@
-module.exports = {
+export default {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    // Optionally add stylistic type-checked rules:
+    // "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
   },
 };
